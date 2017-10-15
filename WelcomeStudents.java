4d890f5784ac484ecc1421b238361b53ee6725df
opopligher1996 <opopligher1996@gmail.com>
2017-10-15 15:41:46 +0800

    Test #3

---
+++ WelcomeStudents.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+import java.io.IOException;
+class WelcomeStudents {
+public static void main(String[] args) {
+	 int val = 9;
+	 System.out.println(val);
+	 System.out.println("You should be able to see this text ");
+	 System.out.println("on screen");
+	 System.out.println("How about this text?");
+	 System.out.println("Can you see the effect?");
+
+}
+}
+
